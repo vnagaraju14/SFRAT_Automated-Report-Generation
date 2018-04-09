a7f0cc2515d51d986b5b981af70769f81680bcef
vnagaraju14 <vnagaraju@umassd.edu>
2018-04-09 16:08:51 -0400

    R markdown script and report specification

---
+++ report-specifications.R
@@ -0,0 +1,18 @@
+#Preliminary
+dataset <- NA #path to the dataset
+
+# TAB 1
+confidence_lvl <- .9 #float
+
+# TAB 2
+models_to_apply <- c('DSS', 'GM', 'Wei','GO','JM') #vector
+mission_time <- 600
+  
+# TAB 3
+num_failures_to_predict <- 1 #integer
+additional_time_software_will_run <- 4116 #float
+desired_reliability <- .9 #float
+reliability_interval_length<- 600 #float
+
+# TAB 4
+percent_data_for_PSEE <- .9
