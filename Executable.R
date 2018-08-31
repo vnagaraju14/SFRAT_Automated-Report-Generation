5ff86fe8abd5bddc36de5a0db04f808eeac5d071
Shekar <vshekar@umassd.edu>
2018-08-31 14:58:15 -0400

    Edited so that only 1 Rmd file is required for verbose and figures only

--- Executable.R
+++ Executable.R
@@ -4,16 +4,16 @@ library(rmarkdown)
 library(readxl)
 source("report-specifications.R")
 
-x <- menu(c("Type '1' for Yes", "Type '0' for No"), title="Display verbose report?")
+x <- menu(c("Type '1' for Yes", "Type '2' for No"), title="Display verbose report?")
 
 if (x==1) {
   rmarkdown::render(paste0(getwd(),'/SFRATReport.Rmd'),  
                     output_file =  paste("SFRAT report_", SheetName,'_', Sys.Date(), ".pdf", sep=''), 
                     output_dir = './Reports')
-} else {
-  rmarkdown::render(paste0(getwd(),'/SFRATReportNoText.Rmd'),  
+} else if (x==2){
+  rmarkdown::render(paste0(getwd(),'/SFRATReport.Rmd'),  
                     output_file =  paste("SFRAT report_NoText_", SheetName,'_', Sys.Date(), ".pdf", sep=''), 
                     output_dir = './Reports')
-}
+} 
 
 
