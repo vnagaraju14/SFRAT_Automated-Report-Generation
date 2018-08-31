719522d3b397f977db0afb22da8814c909f5649d
vnagaraju14 <vnagaraju@umassd.edu>
2018-08-31 16:53:06 -0400

    Added verbose report and data subset option
    Combined executable.R functionalities and added data sub setting option

--- report-specifications.R
+++ report-specifications.R
@@ -1,7 +1,13 @@
-#Preliminary
-datapath <- paste0(getwd(), '/SFRAT/model_testing/SYS1_data.xlsx')#path to the dataset
+library(knitr)
+library(markdown)
+library(rmarkdown)
+library(readxl)
+library(formatR)
 
-dataset <- read_excel(datapath,sheet = 1)  #Specify the sheet with the data
+#Preliminary
+datapath <- paste0(getwd(), '/SFRAT/model_testing/model_data.xlsx')#path to the dataset
+
+datasettemp <- read_excel(datapath,sheet = 1)  #Specify the sheet with the data
 SheetName<-excel_sheets(path=datapath)[1] #Specify the sheet selected in the above line
 colors <- c("navy","red","green","firebrick4","magenta")
 # TAB 1
@@ -21,3 +27,31 @@ reliability_interval_length<- 600 #float
 # TAB 4
 
 percent_data_for_PSSE <- .9
+
+#Verbose report
+
+x <- menu(c("Type '1' for Yes", "Type '2' for No"), title="Display verbose report?")
+
+
+xy <- readline(prompt="Subset data? Type '1' for Yes and '0' for No___")
+
+if (xy==1) {
+  ds <- readline(prompt="How many percent of data should be used in each subset? (Input should be between 0 to 1)___")
+  ds <- as.double(ds) 
+  dsi <- ds
+  while(dsi<=1){
+    dataset <- datasettemp[1:floor(dsi*dim(datasettemp)[1]),]
+    rmarkdown::render(paste0(getwd(),'/SFRATReport.Rmd'),  
+                      output_file =  paste("SFRAT report_", SheetName,'_',dsi,'_', Sys.Date(), ".pdf", sep=''), 
+                      output_dir = './Reports') 
+    
+    dsi=dsi+ds;
+  }
+  
+  
+  
+} else {
+  dataset <- read_excel(datapath,sheet = 1)
+}
+
+
