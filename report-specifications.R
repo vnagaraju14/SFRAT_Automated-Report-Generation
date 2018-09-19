cf1077a7a762a261218e7405aca51caee802ad4b
vnagaraju14 <vnagaraju@umassd.edu>
2018-09-19 00:22:08 -0400

    Commented data subsetting

--- report-specifications.R
+++ report-specifications.R
@@ -9,13 +9,13 @@ library(gdata)
 library(ggplot2)
 library(rootSolve)
 
+datapath <- paste0(getwd(), '/SFRAT/model_testing/SYS1_data.xlsx')   #path to the dataset
+sheetNumber <- 1
 
-#Preliminary
-datapath <- paste0(getwd(), '/SFRAT/model_testing/SYS1_data.xlsx')#path to the dataset
-
-datasettemp <- read_excel(datapath,sheet = 1)  #Specify the sheet with the data
-SheetName<-excel_sheets(path=datapath)[1] #Specify the sheet selected in the above line
+datasettemp <- read_excel(datapath,sheet = sheetNumber)  #Specify the sheet with the data
+SheetName<-excel_sheets(path=datapath)[sheetNumber] #Specify the sheet selected in the above line
 colors <- c("navy","red","green","firebrick4","magenta")
+
 # TAB 1
 confidence_lvl <- .9 #float
 
@@ -31,36 +31,27 @@ desired_reliability <- .9 #float
 reliability_interval_length<- 600 #float
 
 # TAB 4
-
 percent_data_for_PSSE <- .9
 
 #Verbose report
-
+dataset <- datasettemp[1:floor(dim(datasettemp)[1]),]
 x <- menu(c("Type '1' for Yes", "Type '2' for No"), title="Display verbose report?")
+rmarkdown::render(paste0(getwd(),'/SFRATReport.Rmd'),output_file =  paste("SFRAT report_", SheetName,'_', Sys.Date(), ".pdf", sep=''),output_dir = './Reports') 
 
 
-xy <- readline(prompt="Subset data? Type '1' for Yes and '0' for No___")
+#Uncomment the below code to use data subsetting option
+#xy <- readline(prompt="Subset data? Type '1' for Yes and '0' for No___")
 
-if (xy==1) {
-  ds <- readline(prompt="How many percent of data should be used in each subset? (Input should be between 0 to 1)___")
-  ds <- as.double(ds) 
-  dsi <- ds
-  while(dsi<=1){
-    dataset <- datasettemp[1:floor(dsi*dim(datasettemp)[1]),]
-    rmarkdown::render(paste0(getwd(),'/SFRATReport.Rmd'),  
-                      output_file =  paste("SFRAT report_", SheetName,'_',dsi,'_', Sys.Date(), ".pdf", sep=''), 
-                      output_dir = './Reports') 
-    
-    dsi=dsi+ds;
-  }
-  
-  
-  
-} else {
-  dataset <- read_excel(datapath,sheet = 1)
-  rmarkdown::render(paste0(getwd(),'/SFRATReport.Rmd'),  
-                    output_file =  paste("SFRAT report_", SheetName,'_', Sys.Date(), ".pdf", sep=''), 
-                    output_dir = './Reports') 
-}
-
-
+#if (xy==1) {
+#  ds <- readline(prompt="How many percent of data should be used in each subset? (Input should be between 0 to 1)___")
+#  ds <- as.double(ds) 
+#  dsi <- ds
+#  while(dsi<=1){
+#    dataset <- datasettemp[1:floor(dsi*dim(datasettemp)[1]),]
+#    rmarkdown::render(paste0(getwd(),'/SFRATReport.Rmd'),output_file =  paste("SFRAT report_", SheetName,'_',dsi,'_', Sys.Date(), ".pdf", sep=''), output_dir = './Reports') 
+#    dsi=dsi+ds;
+#  }
+#  } else {
+#  dataset <- read_excel(datapath,sheet = 1)
+#  rmarkdown::render(paste0(getwd(),'/SFRATReport.Rmd'), output_file =  paste("SFRAT report_", SheetName,'_', Sys.Date(), ".pdf", sep=''),output_dir = './Reports') 
+#}
