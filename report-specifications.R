08247013cdeb85300d9d98efb1bba022407d9c9f
Josh <41973877+jsteak@users.noreply.github.com>
2018-10-28 19:48:47 -0400

    made script easier to edit/read

--- report-specifications.R
+++ report-specifications.R
@@ -1,33 +1,45 @@
 # edit below for script parameters #
+	
+#subset data into parts? 1 for yes, 0 for no
+subset_data <- 0
 
+#1 for verbose report, 2 for non-verbose
+verbose_report <- 2									
 
+#select which sheet to pick, in this case gives SYS1
+sheetNumber <- 1
+#designate location of data file
+filePath <- '/SFRAT/model_testing/model_data.xlsx'
 
-sheetNumber <- 1									#select which sheet to pick
-filePath <- '/SFRAT/model_testing/model_data.xlsx'	#designate file location
+#Tab 1 Parameters:
+	#laplace test confidence level
+	confidence_lvl <- 0.9
 
-#Tab 1
-confidence_lvl <- 1									#laplace test confidence level
+#Tab 2 Parameters:
+	#number of future failures to predict
+	num_failures_future_prediction <- 2	
 
-#Tab 2
-num_failures_future_prediction <- 3					#number of future failures to predict
-models_to_apply <- c('DSS', 'GM', 'Wei','GO','JM')	#models included, by default is all
-mission_time <- 600									#given mission time
+	#pick models to include, by default is all
+	models_to_apply <- c('DSS', 'GM', 'Wei','GO','JM')
+	#mission time to compute reliability growth
+	mission_time <- 600
 
-#Tab 3
-num_failures_to_predict <- 3						#number of future failures to predict
-additional_time_software_will_run <- 4116 			#future prediction time
-desired_reliability <- .9 							#between 0-1, desired software reliability
-reliability_interval_length<- 600 					#interval size
+#Tab 3 Parameters:
+	#number of future failures to predict
+	num_failures_to_predict <- 3
 
-#Tab 4
-percent_data_for_PSSE <- .9							#predictive sum of squares, percentage
+	#future prediction time
+	additional_time_software_will_run <- 4116
 
+	#between 0-1, desired software reliability
+	desired_reliability <- .9
 
-verbose_report <- 2									#1 for verbose report, 2 for non-verbose
+	#interval size
+	reliability_interval_length<- 600
 
-
-
-
+#Tab 4 Parameters:
+	#predictive sum of squares, percentage
+	percent_data_for_PSSE <- .9
 
 #do not edit below --------------------------------------------------------------------------------------#
 
@@ -48,26 +60,20 @@ SheetName<-excel_sheets(path=datapath)[sheetNumber] #Specify the sheet selected 
 colors <- c("navy","red","green","firebrick4","magenta")
 
 
-
-#Verbose report
 dataset <- datasettemp[1:floor(dim(datasettemp)[1]),]
-x <- verbose_report #1 yes 2 no
+x <- verbose_report
 rmarkdown::render(paste0(getwd(),'/SFRATReport.Rmd'),output_file =  paste("SFRAT report_", SheetName,'_', Sys.Date(), ".pdf", sep=''),output_dir = './Reports') 
 
-
-#Uncomment the below code to use data subsetting option
-#xy <- readline(prompt="Subset data? Type '1' for Yes and '0' for No___")
-
-#if (xy==1) {
-#  ds <- readline(prompt="How many percent of data should be used in each subset? (Input should be between 0 to 1)___")
-#  ds <- as.double(ds) 
-#  dsi <- ds
-#  while(dsi<=1){
-#    dataset <- datasettemp[1:floor(dsi*dim(datasettemp)[1]),]
-#    rmarkdown::render(paste0(getwd(),'/SFRATReport.Rmd'),output_file =  paste("SFRAT report_", SheetName,'_',dsi,'_', Sys.Date(), ".pdf", sep=''), output_dir = './Reports') 
-#    dsi=dsi+ds;
-#  }
-#  } else {
-#  dataset <- read_excel(datapath,sheet = 1)
-#  rmarkdown::render(paste0(getwd(),'/SFRATReport.Rmd'), output_file =  paste("SFRAT report_", SheetName,'_', Sys.Date(), ".pdf", sep=''),output_dir = './Reports') 
-#}
+if (subset_data==1) {
+  ds <- readline(prompt="How many percent of data should be used in each subset? (Input should be between 0 to 1)___")
+  ds <- as.double(ds) 
+  dsi <- ds
+  while(dsi<=1){
+    dataset <- datasettemp[1:floor(dsi*dim(datasettemp)[1]),]
+    rmarkdown::render(paste0(getwd(),'/SFRATReport.Rmd'),output_file =  paste("SFRAT report_", SheetName,'_',dsi,'_', Sys.Date(), ".pdf", sep=''), output_dir = './Reports') 
+    dsi=dsi+ds;
+  }
+  } else {
+  dataset <- read_excel(datapath,sheet = 1)
+  rmarkdown::render(paste0(getwd(),'/SFRATReport.Rmd'), output_file =  paste("SFRAT report_", SheetName,'_', Sys.Date(), ".pdf", sep=''),output_dir = './Reports') 
+}
